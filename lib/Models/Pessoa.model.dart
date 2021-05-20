3f3ac3568badb3ed1129bff67e87c62006e10c54
TulioAvanco <51804367+TulioAvanco@users.noreply.github.com>
2021-05-20 10:36:16 -0300

    Controler e Model de Pessoa

---
+++ lib/Models/Pessoa.model.dart
@@ -0,0 +1,16 @@
+class Pessoa {
+  String? idPessoa;
+  String? senha;
+  String? nome;
+  String? email;
+  String? telefone;
+  int status;
+
+  Pessoa(
+      {this.idPessoa,
+      this.senha,
+      this.nome,
+      this.email,
+      this.telefone,
+      this.status = 0});
+}
