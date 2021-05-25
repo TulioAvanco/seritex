b33507a22712aaad78e61ab0b018dd0d75c1f6cf
TulioAvanco <tulioavanco12@gmail.com>
2021-05-25 13:17:06 -0300

    25 mai

--- lib/Views/cotacao.page.dart
+++ lib/Views/cotacao.page.dart
@@ -53,7 +53,7 @@ class _CotacaoState extends State<Cotacao> {
                               );
                             }).toList(),
                           ),
-                        )
+                        ),
                       ],
                     ),
                   )
