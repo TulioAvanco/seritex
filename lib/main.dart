35e1dcfdae404ec141c3aac85c814c21771f21da
TulioAvanco <tulioavanco12@gmail.com>
2021-05-25 15:08:07 -0300

    Firebase.initializeApp();

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:seritex/Views/app.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
