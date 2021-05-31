bc20282db754342cb6af2d779abcda6588e0a4de
TulioAvanco <tulioavanco12@gmail.com>
2021-05-31 12:22:55 -0300

    botao nova entrega, proxima entrega e ultima entrega adicionados

--- lib/Views/homeAdmin.page.dart
+++ lib/Views/homeAdmin.page.dart
@@ -6,6 +6,8 @@ class HomeAdm extends StatefulWidget {
   _HomeAdmState createState() => _HomeAdmState();
 }
 
+Future<dynamic> novaEntrega(BuildContext context) async {}
+
 class _HomeAdmState extends State<HomeAdm> {
   @override
   Widget build(BuildContext context) {
@@ -102,10 +104,120 @@ class _HomeAdmState extends State<HomeAdm> {
                         border: Border.all(
                             width: 3, color: Color.fromARGB(255, 25, 118, 70)),
                       ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Icon(
+                      Icons.price_change,
+                      color: Color.fromARGB(255, 25, 118, 70),
                     )
                   ],
                 ),
               ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 28)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Proxima Entrega',
+                style: TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 25, 118, 70)),
+              )
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(top: 32)),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Dias',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(35.0),
+                        child: Text('teste'),
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            width: 3, color: Color.fromARGB(255, 25, 118, 70)),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Icon(
+                      Icons.next_plan,
+                      color: Color.fromARGB(255, 25, 118, 70),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Kilos',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(35.0),
+                        child: Text('teste'),
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            width: 3, color: Color.fromARGB(255, 25, 118, 70)),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Icon(
+                      Icons.monitor_weight,
+                      color: Color.fromARGB(255, 25, 118, 70),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Previsão',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(35.0),
+                        child: Text('teste'),
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            width: 3, color: Color.fromARGB(255, 25, 118, 70)),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 16)),
+                    Icon(
+                      Icons.insights,
+                      color: Color.fromARGB(255, 25, 118, 70),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 50)),
+          Container(
+            margin: EdgeInsets.only(left: 50, right: 50),
+            child: ElevatedButton(
+              onPressed: () => novaEntrega(context),
+              child: Text('Nova Entrega'),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 25, 118, 70))),
             ),
           )
         ],
